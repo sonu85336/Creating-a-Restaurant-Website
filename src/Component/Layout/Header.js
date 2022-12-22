6df9ebfa2e893e8db8b365426425203a3653bc58
sonu85336 <sonukushwaha098@gmail.com>
2022-12-22 12:52:19 +0530

    task1

---
+++ src/Component/Layout/Header.js
@@ -0,0 +1,19 @@
+import React,{Fragment} from 'react'
+import HeaderCartButton from './HeaderCartButton';
+import classes from "./Header.module.css";
+import mealsImage from '../../assests/meals.jpg'
+function Header() {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1> Delicious Meals</h1>
+        <HeaderCartButton></HeaderCartButton>
+         
+      </header>
+      <div  className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!'   ></img>
+      </div>
+    </Fragment> 
+  );
+}
+export default Header;
